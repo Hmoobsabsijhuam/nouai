61136a232ee04271434160331cc2494bd34ddb73
Tus Nus Channel <noukhaconnor123@gmail.com>
2025-11-16 23:57:39 +0000

    Because now I can't use Firebase storage then that's why I wanted to use

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -350,7 +350,7 @@ export default function ProfilePage() {
                     render={({ field }) => (
                       <FormItem>
                         <FormLabel>Marital Status</FormLabel>
-                        <Select onValueChange={field.onChange} value={field.value || ''}>
+                        <Select onValueChange={field.onChange} value={field.value ?? ''}>
                           <FormControl>
                             <SelectTrigger>
                               <SelectValue placeholder="Select a status" />
