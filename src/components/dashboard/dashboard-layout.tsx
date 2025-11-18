5544dff2f8600a967564cd09889ca7e2c898675e
Tus Nus Channel <noukhaconnor123@gmail.com>
2025-11-18 01:37:34 +0000

    I still cannot see the full screen on the mobile version

--- src/components/dashboard/dashboard-layout.tsx
+++ src/components/dashboard/dashboard-layout.tsx
@@ -215,7 +215,7 @@ export function DashboardLayout({
           </SidebarFooter>
         </Sidebar>
 
-        <div className="flex flex-1 flex-col overflow-hidden">
+        <div className="flex flex-1 flex-col min-w-0">
             <Header />
             <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
                 {children}
